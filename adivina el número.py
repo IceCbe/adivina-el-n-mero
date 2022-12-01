3779ac41b312e38a1adb3e9fd7cef5edcf6979b7
IceCbe <dplana61@icloud-com>
2022-12-01 20:28:11 +0100

    Muchos errores pero esqueleto hecho. Falta, falta...

--- adivina el número.py
+++ adivina el número.py
@@ -13,7 +13,7 @@ def pista(aleatori_argument, numerointroduit):
     elif (aleatori_pista=="Quadrat"):
         print("\nPista: Es tracta d'un número el quadrat del qual és: " + str(aleatori_argument*aleatori_argument))
     elif(aleatori_pista=="Xifres"):
-        print("\nPista: Es tracta d'un número amb " + len(str(aleatori_argument)) + " xifres")
+        print(("\nPista: Es tracta d'un número amb ") + str(len(str(aleatori_argument))) + (" xifres"))
     else:
         if (aleatori_argument>numerointroduit):
             print("\nProva amb un número més gran")
@@ -22,27 +22,40 @@ def pista(aleatori_argument, numerointroduit):
 
 def menu():
     print("\nBenvingut a adivina el número!")
-    opcio_menu=(int(input(("\nElegeix una opció:\n1-Jugar\n2-Consultar puntuació\n3-Regles del joc\n4-Crèdits\n5-Sortir\n"))))
+    opcio_menu=(int(input(("\nEscull una opció:\n\n1-Jugar\n2-Consultar puntuació\n3-Regles del joc\n4-Crèdits\n5-Sortir\n"))))
     return opcio_menu
 
-def jugar(puntuacio):
+def jugar(puntuacio, impossible):
     sortir=False
     while (sortir==False):
         oportunitats=3
         intents=1
         endevinat=False
         print("\nSelecciona dificultat: \n")
-        dif=int(input("1-Fàcil: Número entre 1 i 15\n2-Normal: Número entre 1 i 25\n3-Difícil: Número entre 1 i 100\n"))
+        if (impossible==False):
+            dif=int(input("1-Fàcil: Número entre 1 i 15\n2-Normal: Número entre 1 i 25\n3-Difícil: Número entre 1 i 100\n"))
+            while(dif<1 or dif>3):
+                print("\nOpció no vàlida.")
+                print("\nSelecciona dificultat: \n")
+                dif=int(input("1-Fàcil: Número entre 1 i 15\n2-Normal: Número entre 1 i 25\n3-Difícil: Número entre 1 i 100\n"))
+        else:
+            dif=int(input("1-Fàcil: Número entre 1 i 15\n2-Normal: Número entre 1 i 25\n3-Difícil: Número entre 1 i 100\n4-Impossible: Número entre 1 i 500\n"))
+            while(dif<1 or dif>4):
+                print("\nOpció no vàlida.")
+                print("\nSelecciona dificultat: \n")
+                dif=int(input("1-Fàcil: Número entre 1 i 15\n2-Normal: Número entre 1 i 25\n3-Difícil: Número entre 1 i 100\n4-Impossible: Número entre 1 i 500\n"))
         if (dif==1):
             aleatori=random.randint(1,15)
             num_max=15
+        elif(dif==2):
+            aleatori=random.randint(1,25)
+            num_max=25
+        elif(dif==3):
+            aleatori=random.randint(1,100)
+            num_max=100
         else:
-            if (dif==2):
-                aleatori=random.randint(1,25)
-                num_max=25
-            else:
-                aleatori=random.randint(1,100)
-                num_max=100
+            aleatori=random.randint(1,500)
+            num_max=500
         while(endevinat==False and oportunitats>0):
             print("\nEt queden " + str(oportunitats) + " oportunitats")
             numerointroduit=int(input("\nIntrodueix un número de l'1 al " + str(num_max) + ": "))
@@ -61,25 +74,27 @@ def jugar(puntuacio):
                 print("\nIncorrecte!")
                 oportunitats-=1
                 intents+=1
-                opcio_pista=str(input("\nVols una pista? (S/N)"))
-                while(opcio_pista!="S" and opcio_pista!="N"):
-                    print("\nOpció no vàlida.")
-                    opcio_pista=int(input("\nVols una pista? (S/N)"))
-                if(opcio_pista=="S"):
-                    pista(aleatori,)
+                if (oportunitats!=0):
+                    opcio_pista=str(input("\nVols una pista? (S/N)"))
+                    while(opcio_pista!="S" and opcio_pista!="N"):
+                        print("\nOpció no vàlida.")
+                        opcio_pista=int(input("\nVols una pista? (S/N)"))
+                    if(opcio_pista=="S"):
+                        pista(aleatori,numerointroduit)
         if (endevinat==True):
             print("\nEnhorabona!!! El número era " + str(aleatori) + ". L'has endevinat en " + str(intents) + " intents.")
         else:
             print("Llàstima!!! El número era " + str(aleatori))
-        tornar=str(input("Vols tornar a jugar? (S/N)"))
+        tornar=str(input("\nVols tornar a jugar? (S/N)"))
         while(tornar!="N" and tornar!="S"):
             print("\nOpció no vàlida.")
-            tornar=str(input("Vols tornar a jugar? (S/N)"))
+            tornar=str(input("\nVols tornar a jugar? (S/N)"))
         if(tornar=="N"):
             print("\nSortint del joc...\n")
             sortir=True
 
 def main():
+    impossible=False
     puntuacio=0
     opcio_menu=menu()
     while(opcio_menu<1 or opcio_menu>5):
@@ -87,12 +102,12 @@ def main():
             opcio_menu=menu()
     while(opcio_menu!=5):
         if (opcio_menu==1):
-            jugar(puntuacio)
+            jugar(puntuacio, impossible)
         elif(opcio_menu==2):
-            #consultar_puntuacio()
-        #elif(opcio_menu==3):
-            #consultar_regles()
-        #else:
+            consultar_puntuacio(puntuacio)
+        elif(opcio_menu==3):
+            consultar_regles()
+        else:
             credits()
     print("\nSortint...\n")
 
@@ -103,7 +118,20 @@ def credits():
         tornar=str(input("\nPrem 'S' per tornar al menú. "))
 
 def consultar_puntuacio(puntuacio):
-    print(("\nLa teva puntuació és: ") + str(puntuacio) + (" punts."))
+    print(("\nLa teva puntuació és: ") + str(puntuacio) + (" punts.\n"))
+    if (puntuacio<10):
+        print(("Amb " ) + str(10-puntuacio) + (" més punts podràs desbloquejar el nivell 'Impossible'"))
+    else:
+        desbloquejar_impossible=str(input("\nVols desbloquejar el nivell 'Impossible'? (S/N) "))
+        if (desbloquejar_impossible=='S'):
+            print("\nNivell impossible desbloquejat. Bona sort.")
+            impossible=True
     tornar=""
     while(tornar!='S'):
         tornar=str(input("\nPrem 'S' per tornar al menú. "))
+    return impossible #PASAR POR PARÁMETRO!!!!!!!!!!!!!!!!!!!!!
+
+def consultar_regles():
+    print("EN DESARROLLO")
+
+main()
